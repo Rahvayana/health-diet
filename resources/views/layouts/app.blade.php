0729c01b070cc48c2baa79337d12deb1d07d2865
rahvayana <fahrursanjaya@gmail.com>
2020-11-22 19:16:07 +0700

    logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -227,9 +227,14 @@
                 <i class="fas fa-cog"></i> Settings
               </a>
               <div class="dropdown-divider"></div>
-              <a href="#" class="dropdown-item has-icon text-danger">
-                <i class="fas fa-sign-out-alt"></i> Logout
-              </a>
+              <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <a class="dropdown-item has-icon text-danger" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                    <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }}
+                </a>
+            </form>
             </div>
           </li>
         </ul>
